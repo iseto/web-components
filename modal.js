9c5a5a1f1d6ffeb40fb5b3f1b50a68430fd76229
Ignatius Seto <ignatius.seto@accenture.com>
2023-04-06 17:39:14 +0200

    section 5: dispatching custom events

--- modal.js
+++ modal.js
@@ -88,6 +88,9 @@ class Modal extends HTMLElement {
 
     cancelButton.addEventListener('click', this._cancel.bind(this))
     confirmButton.addEventListener('click', this._confirm.bind(this))
+    // cancelButton.addEventListener('cancel', () => {
+    //   console.log('cancel inside the component')
+    // })
   }
 
   // attributeChangedCallback(name, oldValue, newValue) {}
@@ -107,12 +110,16 @@ class Modal extends HTMLElement {
     this.isOpen = false
   }
 
-  _cancel() {
+  _cancel(event) {
     this.hide()
+    const cancelEvent = new Event('cancel')
+    event.target.dispatchEvent(cancelEvent)
   }
 
-  _confirm() {
+  _confirm(event) {
     this.hide()
+    const confirmEvent = new Event('confirm')
+    event.target.dispatchEvent(confirmEvent)
   }
 }
 
