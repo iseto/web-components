df4fa623f2189221ab8cf73fb47845344eba6831
Ignatius Seto <ignatius.seto@accenture.com>
2023-04-14 15:08:18 +0200

    section 7: using props in combination with attributes

--- web-components-stencil/src/components.d.ts
+++ web-components-stencil/src/components.d.ts
@@ -21,6 +21,7 @@ export namespace Components {
         "middle": string;
     }
     interface UcSideDrawer {
+        "open": boolean;
         "title": string;
     }
 }
@@ -58,6 +59,7 @@ declare namespace LocalJSX {
         "middle"?: string;
     }
     interface UcSideDrawer {
+        "open"?: boolean;
         "title"?: string;
     }
     interface IntrinsicElements {
