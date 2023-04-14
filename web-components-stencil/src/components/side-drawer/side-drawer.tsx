b27d5038cf67d040a2106fc7c47d12385b70df6a
Ignatius Seto <ignatius.seto@accenture.com>
2023-04-14 16:15:11 +0200

    section 7: add more event listeners and use state

--- web-components-stencil/src/components/side-drawer/side-drawer.tsx
+++ web-components-stencil/src/components/side-drawer/side-drawer.tsx
@@ -1,4 +1,4 @@
-import { Component, Prop, h } from '@stencil/core';
+import { Component, Prop, State, h } from '@stencil/core';
 
 @Component({
   tag: 'uc-side-drawer',
@@ -6,6 +6,7 @@ import { Component, Prop, h } from '@stencil/core';
   shadow: true,
 })
 export class SideDrawer {
+  @State() showContactInfo = false;
   @Prop({ reflect: true }) title: string;
   @Prop({ reflect: true, mutable: true }) open: boolean;
 
@@ -13,38 +14,65 @@ export class SideDrawer {
     this.open = false;
   }
 
-  render() {
-    let content = null;
+  onContentChange(content: string) {
+    console.log(content);
+    this.showContactInfo = content == 'contact';
+  }
 
-    if (this.open) {
-      content = (
-        <aside>
-          <header>
-            <h1>{this.title}</h1>
-            <button onClick={this.onCloseDrawer.bind(this)}>X</button>
-          </header>
-          <section id="tabs">
-            <button class="active">Navigation</button>
-            <button>Contact</button>
-          </section>
-          <main>
-            <slot />
-          </main>
-        </aside>
+  render() {
+    // let content = null;
+    let mainContent = <slot />;
+
+    if (this.showContactInfo) {
+      mainContent = (
+        <div>
+          <h2>Contact Information</h2>
+          <p>You can reach us via phone or email.</p>
+          <ul>
+            <li>Phone : 49802384032</li>
+            <li>
+              E-Mail:{''}
+              <a href="mailto:something@something.com">something@something.com</a>
+            </li>
+          </ul>
+        </div>
       );
     }
 
-    return content;
-    // return (
-    //   <aside>
-    //     <header>
-    //       <h1>{this.title}</h1>
-    //       <button>X</button>
-    //     </header>
-    //     <main>
-    //       <slot />
-    //     </main>
-    //   </aside>
-    // );
+    // if (this.open) {
+    //   content = (
+    //     <aside>
+    //       <header>
+    //         <h1>{this.title}</h1>
+    //         <button onClick={this.onCloseDrawer.bind(this)}>X</button>
+    //       </header>
+    //       <section id="tabs">
+    //         <button class="active">Navigation</button>
+    //         <button>Contact</button>
+    //       </section>
+    //       <main>
+    //         <slot />
+    //       </main>
+    //     </aside>
+    //   );
+    // }
+
+    return (
+      <aside>
+        <header>
+          <h1>{this.title}</h1>
+          <button onClick={this.onCloseDrawer.bind(this)}>X</button>
+        </header>
+        <section id="tabs">
+          <button class={!this.showContactInfo ? 'active' : ''} onClick={this.onContentChange.bind(this, 'nav')}>
+            Navigation
+          </button>
+          <button class={this.showContactInfo ? 'active' : ''} onClick={this.onContentChange.bind(this, 'contact')}>
+            Contact
+          </button>
+        </section>
+        <main>{mainContent}</main>
+      </aside>
+    );
   }
 }
