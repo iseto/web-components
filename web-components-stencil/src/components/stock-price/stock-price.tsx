e40d7f3964976d92fab9021a5c8cfc277cf665a8
Ignatius Seto <ignatius.seto@accenture.com>
2023-04-16 22:37:04 +0200

    section 8: add fetch method and demo URL

--- web-components-stencil/src/components/stock-price/stock-price.tsx
+++ web-components-stencil/src/components/stock-price/stock-price.tsx
@@ -1,8 +1,35 @@
-import { Component } from '@stencil/core';
+import { Component, State, h } from '@stencil/core';
 
 @Component({
   tag: 'uc-stock-price',
   styleUrl: './stock-price.css',
   shadow: true,
 })
-export class StockPrice {}
+export class StockPrice {
+  @State() fetchedPrice: number;
+
+  onFetchStockSubmit(event: Event) {
+    event.preventDefault();
+    console.log('submitted!');
+    fetch('https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=MSFT&apikey=demo')
+      .then(res => res.json())
+      .then(parsedRes => {
+        this.fetchedPrice = +parsedRes['Global Quote']['05. price'];
+      })
+      .catch(error => console.log(error));
+  }
+
+  render() {
+    return [
+      <form onSubmit={this.onFetchStockSubmit.bind(this)}>
+        <div>
+          <input type="text" id="stock-symbol" />
+          <button type="submit">Fetch</button>
+        </div>
+      </form>,
+      <div>
+        <p>Price: ${this.fetchedPrice}</p>
+      </div>,
+    ];
+  }
+}
