85497f58258f1bd718a9300abc99cf1f82832b1a
Ignatius Seto <ignatius.seto@accenture.com>
2023-05-25 21:40:37 +0200

    section 8: outputting a list of elements and style them

--- web-components-stencil/src/components/stock-finder/stock-finder.tsx
+++ web-components-stencil/src/components/stock-finder/stock-finder.tsx
@@ -1,4 +1,4 @@
-import { Component, h } from '@stencil/core';
+import { Component, State, h } from '@stencil/core';
 
 import { AV_API_KEY } from '../../global';
 
@@ -10,13 +10,24 @@ import { AV_API_KEY } from '../../global';
 export class StockFinder {
   stockNameInput: HTMLInputElement;
 
+  @State() searchResults: { symbol: string; name: string }[] = [];
+
   onFindStocks(e) {
     e.preventDefault();
     const stockName = this.stockNameInput.value;
     fetch(`https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=${stockName}&apikey=${AV_API_KEY}`)
       .then(res => res.json())
-      .then(parsedRes => console.log(parsedRes))
-      .catch();
+      .then(parsedRes => {
+        console.log(parsedRes['bestMatches']);
+        this.searchResults = parsedRes['bestMatches'].map(match => {
+          return {
+            name: match['2. name'],
+            symbol: match['1. symbol'],
+          };
+        });
+        console.log(this.searchResults);
+      })
+      .catch(err => console.log(err));
   }
 
   render() {
@@ -29,6 +40,13 @@ export class StockFinder {
           </button>
         </div>
       </form>,
+      <ul>
+        {this.searchResults.map(result => (
+          <li>
+            <strong>{result.symbol}</strong> - {result.name}
+          </li>
+        ))}
+      </ul>,
     ];
   }
 }
